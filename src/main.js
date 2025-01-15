1b7c813dc0e875a55146f6d0defeffc68debb70d
Lucas Ballaty <52033024+lerda8@users.noreply.github.com>
2025-01-15 14:14:32 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import './app.postcss';
+import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+import '@skeletonlabs/skeleton/styles/skeleton.css';
 import App from './App.svelte';
 
 const app = new App({
