488b1398d7bbb0180bf725b4b839815e0a550238
Lucas Ballaty <52033024+lerda8@users.noreply.github.com>
2025-01-15 14:11:33 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
+import sveltePreprocess from 'svelte-preprocess';
+import postcss from 'rollup-plugin-postcss';
 
 export default {
   input: 'src/main.js',
@@ -11,10 +13,18 @@ export default {
   },
   plugins: [
     svelte({
+      preprocess: sveltePreprocess({
+        postcss: true,
+      }),
       compilerOptions: {
         dev: true
       },
       emitCss: true
+    }),
+    postcss({
+      config: {
+        path: './postcss.config.js'
+      }
     }),
     css({ output: 'bundle.css' }),
     resolve({
